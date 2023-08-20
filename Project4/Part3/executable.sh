6084dca3e48d85d5ff048dcce9e98ea9d62b73da
Ehsan Naghavi <59973930+samanngh@users.noreply.github.com>
2023-08-20 11:56:27 -0400

    Create executable.sh

---
+++ Project4/Part3/executable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+N=1000
+
+date=$(date '+%Y-%m-%d')
+
+# Always in bytes
+for(( size = 1; size <=4; size *= 2))
+do
+    for(( threads = 1; threads <= 32; threads *= 2 ))
+    do
+        echo "-----------------------------------------------"
+        echo "Running for number of threads:$threads and nodes:$size"
+        echo "-----------------------------------------------"
+        mpirun -n $size ./part3-OMP $threads $N
+    done
+done
